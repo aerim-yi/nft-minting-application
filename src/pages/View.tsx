8ce29cf425527a0ad54a1fa3351ad7eaea415b10
aerim-yi <aerim.yi@immutable.com>
2022-08-30 21:11:52 +1000

    feat: call asset api to retrieve data

--- src/pages/View.tsx
+++ src/pages/View.tsx
@@ -1,24 +1,32 @@
 import { useState, useEffect } from 'react';
 import { getUserProjects } from '../api/projects-api';
+import { getUserAssets } from '../api/assets-api'
 import { Container, Row, Col, Table } from 'react-bootstrap';
-import { Project, signRaw } from '@imtbl/core-sdk';
+import { Project, signRaw, Asset } from '@imtbl/core-sdk';
 import { ethers } from 'ethers';
+import placeholderImg from '../asset/placeholderImg.png'
 
 export const View = () => {
+    const wallet = localStorage.WALLET_ADDRESS
     const [projects, setProjects] = useState<Project[]>()
+    const [assets, setAssets] = useState<Asset[]>()
 
 
     useEffect(() => {
         const getSinger = async () => {
+            //get Signature from Metamask wallet
             const provider = new ethers.providers.Web3Provider(window.ethereum);
             await provider.send('eth_requestAccounts', []); //@TODO: ask Calvin why (https://stackoverflow.com/questions/71198438/ethers-js-error-unknown-account-0-operation-getaddress-code-unsupported-o)
-            const signer = provider.getSigner();  //get Signature from Metamask wallet
+            const signer = provider.getSigner();
 
             const timestamp = Math.floor(Date.now() / 1000).toString();
             const signature = await signRaw(timestamp, signer); // IMX-Signature
 
-            const response = await getUserProjects(signature, timestamp)
-            if (response) setProjects(response.result)
+            const pojectResponse = await getUserProjects(signature, timestamp)
+            if (pojectResponse) setProjects(pojectResponse.result)
+
+            const assetResponse = await getUserAssets(wallet)
+            if (assetResponse) setAssets(assetResponse.result)
         }
         getSinger()
     }, [])
@@ -31,7 +39,7 @@ export const View = () => {
                     <Table striped bordered hover>
                         <thead>
                             <tr>
-                                <th rowSpan={2}>id</th>
+                                <th rowSpan={2}>Id</th>
                                 <th rowSpan={2}>Company</th>
                                 <th colSpan={2}>Collection</th>
                                 <th colSpan={2}>Mint</th>
@@ -58,6 +66,38 @@ export const View = () => {
                                     )
                                 })}
                                 {!projects &&
+                                    <h5>Loading...</h5>
+                                }
+                            </>
+                        </tbody>
+                    </Table>
+                </Col>
+            </Row>
+            <Row>
+                <Col>
+                    <h3>My Assets</h3>
+                    <Table striped bordered hover>
+                        <thead>
+                            <tr>
+                                <th>Token ID</th>
+                                <th>Name</th>
+                                <th>Collection Name</th>
+                                <th>Image</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <>
+                                {assets && assets.map((asset) => {
+                                    return (
+                                        <tr>
+                                            <td>{asset.token_id}</td>
+                                            <td>{asset.name}</td>
+                                            <td>{asset.collection.name}</td>
+                                            <td><img src={asset.image_url || placeholderImg} alt="" style={{ maxWidth: 50 }}></img></td>
+                                        </tr>
+                                    )
+                                })}
+                                {!assets &&
                                     <h5>Loading...</h5>
                                 }
                             </>
