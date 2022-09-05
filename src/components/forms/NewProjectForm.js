cfdd206b1007bb3b288d38b7708da3d8c2876f5d
aerim-yi <aerim.yi@immutable.com>
2022-09-05 17:07:54 +1000

    feat: add onChange and onClick event handlers

--- src/components/forms/NewProjectForm.js
+++ src/components/forms/NewProjectForm.js
@@ -1,21 +1,40 @@
+import { useState } from 'react';
+import { createProject } from '../../api/projects-api';
+import { ethers } from 'ethers';
 import { Button, Form } from 'react-bootstrap';
 
-export function NewProjectForm() {
+export const NewProjectForm = () => {
+  const [projectName, setProjectName] = useState();
+  const [companyName, setCompanyName] = useState();
+  const [contactEmail, setContactEmail] = useState();
+  const [projectId, setProjectId] = useState();
+
+  const createNewProject = async () => {
+    const provider = new ethers.providers.Web3Provider(window.ethereum);
+    await provider.send('eth_requestAccounts', []);
+    const signer = provider.getSigner();
+
+    const project = await createProject(signer, companyName, contactEmail, projectName)
+    if (project.id) {
+      setProjectId(project.id)
+    }
+  }
+
   return (
     <Form>
       <Form.Group className="mb-3">
         <Form.Label>Project Name</Form.Label>
-        <Form.Control placeholder="Enter project name" />
+        <Form.Control placeholder="Enter project name" onChange={e => setProjectName(e.target.value)} />
       </Form.Group>
       <Form.Group className="mb-3">
         <Form.Label>Company Name</Form.Label>
-        <Form.Control placeholder="Enter company name" />
+        <Form.Control placeholder="Enter company name" onChange={e => setCompanyName(e.target.value)} />
       </Form.Group>
       <Form.Group className="mb-3" controlId="formBasicEmail">
         <Form.Label>Contact Email</Form.Label>
-        <Form.Control type="email" placeholder="Enter email" />
+        <Form.Control type="email" placeholder="Enter email" onChange={e => setContactEmail(e.target.value)} />
       </Form.Group>
-      <Button variant="primary" type="submit">
+      <Button variant="primary" type="button" onClick={createNewProject}>
         Create Project
       </Button>
     </Form>
