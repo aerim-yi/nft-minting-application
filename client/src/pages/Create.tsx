eaae61883bdb2d1f7faf90f2282239509337c80a
aerim-yi <aerim.yi@immutable.com>
2022-09-08 19:47:53 +1000

    feat: display data in the information board

--- client/src/pages/Create.tsx
+++ client/src/pages/Create.tsx
@@ -1,33 +1,40 @@
+import { useState } from 'react';
 import { Container, Row, Col, Accordion, Alert } from 'react-bootstrap';
 import { DeploySmartContractForm } from '../components/forms/DeploySmartContractForm';
+import { NewCollectionForm } from '../components/forms/NewCollectionForm';
 import { NewProjectForm } from '../components/forms/NewProjectForm';
 
 export const Create = () => {
+    const [alerts, setAlerts] = useState([]);
+
+    const setInfo = (item) => {
+        setAlerts([...alerts, item])
+    }
+
     return (
         <Container>
             <Row>
                 <Col>
                     <Alert variant="info" className="mt-3">
                         <Alert.Heading>Information Board</Alert.Heading>
-                        <p>
-                            No data available yet
-                        </p>
-                        <hr />
-                        <p className="mb-0">
-                            No data available yet
-                        </p>
+                        {alerts.length === 0 && <p>No data available yet</p>}
+                        {alerts.length > 0 && <ul>
+                            {alerts.map((item, index) => (
+                                <li key={index}>{`${item.key}: ${item.value}`}</li>
+                            ))}
+                        </ul>}
                     </Alert>
                     <Accordion defaultActiveKey="0" className="mt-3">
                         <Accordion.Item eventKey="0">
                             <Accordion.Header>1. Deploy Smart Contract</Accordion.Header>
                             <Accordion.Body>
-                                <DeploySmartContractForm />
+                                <DeploySmartContractForm setInfo={setInfo} />
                             </Accordion.Body>
                         </Accordion.Item>
                         <Accordion.Item eventKey="1">
                             <Accordion.Header>2. Create New Project</Accordion.Header>
                             <Accordion.Body>
-                                <NewProjectForm />
+                                <NewProjectForm setInfo={setInfo} />
                             </Accordion.Body>
                         </Accordion.Item>
                         <Accordion.Item eventKey="2">
@@ -42,10 +49,7 @@ export const Create = () => {
                         <Accordion.Item eventKey="3">
                             <Accordion.Header>4. Create New Collection</Accordion.Header>
                             <Accordion.Body>
-                                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-                                minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-                                aliquip ex ea commodo consequat.
+                                <NewCollectionForm />
                             </Accordion.Body>
                         </Accordion.Item>
                         <Accordion.Item eventKey="4">
