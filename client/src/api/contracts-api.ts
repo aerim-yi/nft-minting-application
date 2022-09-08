3f9d1a74f1e3cea3e7a651a3effb22e9cfb6afdc
aerim-yi <aerim.yi@immutable.com>
2022-09-08 10:49:07 +1000

    feat: call api to retrieve abi and bytecode

--- client/src/api/contracts-api.ts
+++ client/src/api/contracts-api.ts
@@ -1,28 +1,41 @@
+// @TODO: clean up code
 import { ethers, ContractFactory } from 'ethers';
 import { getIMXAddress, getNetwork } from "../deploy/utils";
 
-const abi = "";
-const bytecode = ""
-
 export const deploySmartContract = async (owner: string, contractName: string, contractSymbol: string) => {
-    const provider = new ethers.providers.Web3Provider(window.ethereum);
-    await provider.send('eth_requestAccounts', []);
-    const signer = provider.getSigner();
+    const response = await fetch("/asset-contract");
+    const contractJSON = await response.json();
 
-    console.log("Deploying Contract with the account:", signer._address);
+    const abi = contractJSON?.abi;
+    const bytecode = contractJSON?.bytecode;
 
-    //  Get chain id from metamask so we can work out what network we are on.
-    const chainId = window.ethereum.networkVersion;
+    if (abi && bytecode) {
+        const provider = new ethers.providers.Web3Provider(window.ethereum);
+        await provider.send('eth_requestAccounts', []);
+        const signer = provider.getSigner();
 
-    //  Resolve network based on chain id.
-    const network = getNetwork(chainId)
-    const imxAddress = getIMXAddress(network);
+        console.log("Deploying Contract with the account:", signer._address);
+        console.log("abi", abi)
+        console.log("bytecode", bytecode)
 
-    const Asset = new ContractFactory(abi, bytecode, signer)
-    const asset = await Asset.deploy(owner, contractName, contractSymbol, imxAddress);
-    await asset.deployed()
-    console.log("Deployed Contract Address:", asset.address);
-    console.log('Verifying contract in 5 minutes...');
+        // Get chain id from metamask so we can work out what network we are on.
+        const chainId = window.ethereum.networkVersion;
 
-    return asset
+        // Resolve network based on chain id.
+        const network = getNetwork(chainId);
+        const imxAddress = getIMXAddress(network);
+
+        const Asset = new ContractFactory(abi, bytecode, signer);
+
+        // Deploy an instance of the contract
+        const asset = await Asset.deploy(owner, contractName, contractSymbol, imxAddress);
+        await asset.deployed();
+        console.log("Deployed Contract Address:", asset.address);
+        console.log('Verifying contract in 5 minutes...');
+
+        return asset
+
+    } else {
+        console.log('Contract ABI and Bytecode does not exsit, please compile your smart contract')
+    }
 }
