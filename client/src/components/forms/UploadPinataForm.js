d6c6f9d963510cf5bb159f8c94ed55c941aab04c
aerim-yi <aerim.yi@immutable.com>
2022-09-23 11:46:07 +1000

    feat: add alert message when the image is uploaded

--- client/src/components/forms/UploadPinataForm.js
+++ client/src/components/forms/UploadPinataForm.js
@@ -1,9 +1,10 @@
 import { useState } from 'react';
-import { Form, Button } from 'react-bootstrap';
+import { Form, Button, Toast } from 'react-bootstrap';
 import axios from "axios";
 
 export const UploadPinataForm = ({ setInfo }) => {
     const [imgFile, setImgFile] = useState();
+    const [show, setShow] = useState(false);
 
     const sendImgFile = async () => {
         const data = new FormData();
@@ -12,11 +13,16 @@ export const UploadPinataForm = ({ setInfo }) => {
         const response = await axios.post('http://localhost:3080/upload', data);
 
         setInfo({ key: "Image URL", value: `https://gateway.pinata.cloud/ipfs/${response.data}` })
+        setShow(true)
         console.log("IMAGES_SUBMIT_SUCCESS");
     }
 
     return (
         <Form>
+            <Toast onClose={() => setShow(false)} show={show} delay={3000} autohide bg={'info'}>
+                <Toast.Body>{`Your image has been uploaded!`}</Toast.Body>
+            </Toast>
+            <br />
             <Form.Group controlId="formFile" className="mb-3">
                 <Form.Label>Select NFT files</Form.Label>
                 <Form.Control type="file" name="file" onChange={e => setImgFile(e.target.files[0])} />
